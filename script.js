908fa6004542b14f4821778cbf9cad3aca7452eb
christianhuntley <huntleychrist@gmail.com>
2018-12-16 23:24:37 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,9 @@
+var btn = document.createElement("BUTTON");        // Create a <button> element
+var t = document.createTextNode("HOME");       // Create a text node
+btn.appendChild(t);                                // Append the text to <button>
+document.body.appendChild(btn);                    // Append <button> to <body>
+
+var btn = document.createElement("BUTTON");        // Create a <button> element
+var t = document.createTextNode("IMAGES");       // Create a text node
+btn.appendChild(t);                                // Append the text to <button>
+document.body.appendChild(btn);                    // Append <button> to <body>
